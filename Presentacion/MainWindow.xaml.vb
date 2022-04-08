de2341b37b547410108b1b61a988de3c2ec08baa
Javier Aparicio <javier.aparicio@audinforsystem.es>
2022-04-08 14:39:54 +0200

    Agregar archivos de proyecto.

---
+++ Presentacion/MainWindow.xaml.vb
@@ -0,0 +1,7 @@
+﻿Class MainWindow
+    Private Sub OnClick_CambiaEstado(sender As Object, e As RoutedEventArgs)
+        Dim ventana As New Window1
+        ventana.Show()
+        Close()
+    End Sub
+End Class
---
+++ Presentacion/Window1.xaml.vb
@@ -0,0 +1,9 @@
+﻿Public Class Window1
+    Private Sub OnClick_Buscar(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub OnClic_AniadirNueva(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+End Class
