ea1d645ffccf18101b15ea42f269c969f7d7dd5a
Sabrina Garcia <sabrina.garcia@audinforsystem.es>
2022-04-12 17:18:53 +0200

    varios

--- Presentacion/Inicio.xaml.vb
+++ Presentacion/Inicio.xaml.vb
@@ -1,36 +1,44 @@
-﻿Public Class Inicio
-    'Dim wc As New WaitControl
+﻿Imports System.Timers
+Imports System.Windows.Media.Animation
+
+Public Class Inicio
+    Dim wc As New WaitControl
     Public Sub New()
         InitializeComponent()
-        'Logo.Children.Add(wc)
-        'Traductor.TraducirIdioma(Me, If(SessionInformationDTO.UsuarioActivo(SessionInformationClientDTO.IdUsuario), New UsuarioDTO()).Culture)
+        Logo.Children.Add(wc)
+
 
     End Sub
 
     Private Sub OnClic_Principal(sender As Object, e As RoutedEventArgs)
         ' System.Threading.Thread.Sleep(3000)
-
+        '
         Dim p As New Peticiones
         p.Show()
         Close()
     End Sub
-
     Private Sub Inicio(sender As Object, e As RoutedEventArgs)
+        Iniciar()
+        For j As Integer = 1 To 10
+            ' System.Threading.Thread.Sleep(1000)
+        Next
+
+        'Cargar()
+    End Sub
+    Private Sub Iniciar()
 
         WaitControl.AnimationOn = True : MainGridFondo.Visibility = Visibility.Hidden
 
-        'Dim sTask As Task = Task.Factory.StartNew(
-        'Sub()
-        'LoadAll()
-        'End Sub)
-        'Await sTask
         WaitControl.AnimationOn = False : MainGridFondo.Visibility = Visibility.Visible
-        WaitControl.AnimationOn = True : System.Threading.Thread.Sleep(1000)
+
+        If  Then
+            Cargar()
+        End If
 
     End Sub
 
     Private Sub Cargar()
-        'System.Threading.Thread.Sleep(10000)
+
         Dim p As New Peticiones
         p.Show()
         Close()
