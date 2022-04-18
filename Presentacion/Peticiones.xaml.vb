d0bb8567a9b0d42b04d335457688081a922f71ce
Sabrina Garcia <sabrina.garcia@audinforsystem.es>
2022-04-18 13:12:00 +0200

    .

--- Presentacion/Peticiones.xaml.vb
+++ Presentacion/Peticiones.xaml.vb
@@ -1,9 +1,20 @@
 ﻿Public Class Peticiones
+    Dim agregarpet As New AgregarPeticion
+    Dim agregarcli As New AgregarCliente
     Private Sub OnClick_Buscar(sender As Object, e As RoutedEventArgs)
 
     End Sub
 
     Private Sub OnClic_AniadirNueva(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub OnClic_AniadirPeticion(sender As Object, e As RoutedEventArgs)
+        agregarpet.Show()
+
+    End Sub
+
+    Private Sub OnClic_AniadirCliente(sender As Object, e As RoutedEventArgs)
 
     End Sub
 End Class
