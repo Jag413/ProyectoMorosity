0553ecdef8cd609ee164a8f088d30fdc57bb0a64
sabrina <sabrina.garcia@audinforsystem.es>
2022-04-25 11:26:56 +0200

    .

---
+++ WorkerServiceScoring/Worker.cs
@@ -0,0 +1,24 @@
+namespace WorkerServiceScoring
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private IConfiguration _configuration;
+
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"Worker secrets RabbitUser: {_configuration["RabbitUser"]}");
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
