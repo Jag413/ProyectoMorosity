8b2d58636349997eb766947d7df41c67cc16850d
Jose Manuel Cuadra <josemanuel.cuadra@audinforsystem.es>
2022-05-03 11:05:44 +0200

    Contexto Code First

---
+++ DAL1StSharp/DAL1stContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL1StSharp
+{
+    public class DAL1stContext : DbContext
+    {
+        public DAL1stContext() : base("DalDB")
+        {
+            Database.SetInitializer<DAL1stContext>(new CreateDatabaseIfNotExists<DAL1stContext>());
+
+            // Database.SetInitializer(new MigrateDatabaseToLatestVersion<DAL1stContext, DAL1stContext.Migrations.Configuration>());
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ DAL1StSharp/Migrations/202205030002457_inicialv1.cs
@@ -0,0 +1,26 @@
+﻿namespace DAL1StSharp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class inicialv1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 20),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Students");
+        }
+    }
+}
