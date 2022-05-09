bd1b1c89e36e0583ad626aeb7a0fb73eebe2c05d
Javier <javier.aparicio@audinforsystem.es>
2022-05-09 11:46:02 +0200

    Crear controler

---
+++ FakeEquifax/Controllers/ConsultaEquifaxController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace FakeEquifax.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class ConsultaEquifaxController : Controller
+    {
+        
+        // GET: ConsultaEquifaxController/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: ConsultaEquifaxController/Create
+        [HttpPost]
+        public ActionResult Create(PersonaScoringBase personaScoring)
+        {
+            try
+            {
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
